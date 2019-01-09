73a50e039e43441c095e2c4199c03f43419a46eb
zhangjingxiang <821940979@qq.com>
2019-01-09 17:48:38 +0800

    下班了，个人详情页，待继续完善

--- src/views/Service/components/personal_info.vue
+++ src/views/Service/components/personal_info.vue
@@ -1,43 +1,90 @@
 <template>
-    <div>
+    <div class="personal">
 
-      <Row>
-        <i-col span="24">
-          <img src="/static/picture/avatar_default.jpg" alt="..." class="img-circle img-thumbnail" style="width: 200px;height: 200px">
-          <Upload action="upload_url"
-                  :before-upload="handleUpload">
-            <Button>上传头像</Button>
-          </Upload>
+      <Row type="flex" justify="center" align="middle" class="code-row-bg">
+        <i-col span="4" style="border: 1px solid red;" class="text-center">
+          <div class="text-center">
+            <img src="/static/picture/avatar_default.jpg" alt="..." class="img-circle img-thumbnail" style="width: 150px;height: 150px;">
+            <Upload action="upload_url"
+                    :before-upload="handleUpload">
+              <Button style="margin-top: 10px">上传头像</Button>
+            </Upload>
+          </div>
         </i-col>
-      </Row>
 
-      <Row>
-        <i-col span="24">
-          <Modal title="请选择喜欢的区域作为你的个人头像" v-model="isShowModal" :mask-closable="false" width="75"
-                 :styles="ModalHeight"
-                 @on-ok="upLoadFiles"
-                 :loading="loadingStatus"
-                 ok-text="上传图片">
-            <div class="cropper-content">
-              <div class="cropper" :style="scrollHeight">
-                <vueCropper class="vueCropper"
-                            ref="cropper"
-                            :img="option.img"
-                            :outputSize="option.outputSize"
-                            :outputType="option.outputType"
-                            :info="option.info"
-                            :canScale="option.canScale"
-                            :autoCrop="option.autoCrop"
-                            :autoCropHeight="option.autoCropHeight"
-                            :auto-crop-width="option.autoCropWidth"
-                            :center-box="option.centerBox"
-                            :can-move="option.canMove"
-                ></vueCropper>
-              </div>
+        <i-col span="10" style="border: 1px solid black;padding: 15px">
+          <div class="userInfo">
+
+            <div>
+              <p><input type="text" v-model="userInfo.nickname" disabled></input></p>
             </div>
-          </Modal>
+
+            <div>
+              <button class="btn btn-info" type="button">
+                 当前活跃度天数： <span class="badge">4</span>
+              </button>
+            </div>
+
+            <div>
+              <span><Icon type="md-pin" /><input type="text" v-model="userInfo.location" disabled></input></span>
+            </div>
+
+            <div>
+              <span><Icon type="ios-pricetags" /> <Tag color="blue"  v-if="userInfo.tags !== null" v-for="user in userInfo.tags">{{user.tag}}</Tag></span>
+            </div>
+
+            <div>
+              <span><Icon type="md-at" /><input type="text" v-model="userInfo.email" disabled></input></span>
+            </div>
+
+            <div>
+              <span><Icon type="md-transgender" /><input type="text" v-model="userInfo.gender" disabled></span>
+            </div>
+
+          </div>
+        </i-col>
+
+        <i-col span="10" style="border: 1px solid orange;" align="top">
+          <Card shadow style="min-height: 250px;max-height: 250px">
+            <p slot="title" style="font-size: 16px;font-weight: normal">
+              <Icon type="ios-film-outline"></Icon>
+              心愿墙
+            </p>
+            <a href="#" slot="extra" @click.prevent="changeLimit">
+              <Icon type="ios-loop-strong"></Icon>
+              编辑
+            </a>
+
+          </Card>
         </i-col>
       </Row>
+
+
+      <!-- **************************************** 图片裁剪 start ***************************************************** -->
+      <Modal title="请选择喜欢的区域作为你的个人头像" v-model="isShowModal" :mask-closable="false" width="75"
+             :styles="ModalHeight"
+             @on-ok="upLoadFiles"
+             :loading="loadingStatus"
+             ok-text="上传图片">
+        <div class="cropper-content">
+          <div class="cropper" :style="scrollHeight">
+            <vueCropper class="vueCropper"
+                        ref="cropper"
+                        :img="option.img"
+                        :outputSize="option.outputSize"
+                        :outputType="option.outputType"
+                        :info="option.info"
+                        :canScale="option.canScale"
+                        :autoCrop="option.autoCrop"
+                        :autoCropHeight="option.autoCropHeight"
+                        :auto-crop-width="option.autoCropWidth"
+                        :center-box="option.centerBox"
+                        :can-move="option.canMove"
+            ></vueCropper>
+          </div>
+        </div>
+      </Modal>
+      <!-- **************************************** 图片裁剪 end ***************************************************** -->
 
     </div>
 </template>
@@ -50,7 +97,6 @@
       data(){
           return {
             file:null,
-            modal:false,
             loadingStatus: true,
             uploadBase64:null,
             option:{
@@ -66,6 +112,18 @@
               canMove:false,                   // 上传图片是否可以移动
             },
             isShowModal:false,  //是否显示对话框
+            userInfo:{
+              nickname:'丿丶祥灬少',
+              location:'成都',
+              tags:[
+                {tag:'java'},
+                {tag:'SpringBoot'},
+                {tag:'vue'},
+                {tag:'Android'}
+              ],
+              email:'821940979@qq.com',
+              gender:'男'
+            }
           }
       },
       methods:{
@@ -91,8 +149,8 @@
         },
 
         upLoadFiles(){
-          alert(6666)
-          this.isShowModal = true;
+          alert(6666);
+          this.isShowModal = false;
         }
 
       },
@@ -112,12 +170,35 @@
 </script>
 
 <style scoped>
+
+  p{
+    font-size: 25px;
+    font-weight: bold;
+  }
+  span{
+    font-size: 18px;
+  }
+
   .cropper-content{
     width: 100%;
     height: 100%;
   }
+
   .cropper{
     width: 100%;
+  }
+
+  .userInfo input{
+    border: 0;
+    /*border: 1px solid red;*/
+    background-color: white;
+    margin: 1px 15px;
+  }
+
+  .userInfo div{
+    margin-top: 5px;
+    margin-bottom: 5px;
+    margin-left: 5px;
   }
 
 </style>
