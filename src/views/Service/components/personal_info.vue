20728e2f48f127dd00f1b2b59ee0260c5fa9cc3e
zhangjingxiang <821940979@qq.com>
2019-01-09 15:25:59 +0800

    初步实现图片的选择，裁剪

--- src/views/Service/components/personal_info.vue
+++ src/views/Service/components/personal_info.vue
@@ -6,35 +6,36 @@
           <img src="/static/picture/avatar_default.jpg" alt="..." class="img-circle img-thumbnail" style="width: 200px;height: 200px">
           <Upload action="upload_url"
                   :before-upload="handleUpload">
-            <Button @click="select_file">上传头像</Button>
+            <Button>上传头像</Button>
           </Upload>
         </i-col>
       </Row>
 
       <Row>
         <i-col span="24">
-          <Modal title="请截取你所需的部分"
-          v-model="isShowModal" v-if="isShowModal">
+          <Modal title="请选择喜欢的区域作为你的个人头像" v-model="isShowModal" :mask-closable="false" width="75"
+                 :styles="ModalHeight"
+                 @on-ok="upLoadFiles"
+                 :loading="loadingStatus"
+                 ok-text="上传图片">
             <div class="cropper-content">
               <div class="cropper" :style="scrollHeight">
-                <vueCropper
-                  ref="cropper"
-                  :img="option.img"
-                  :outputSize="option.outputSize"
-                  :outputType="option.outputType"
-                  :info="option.info"
-                  :canScale="option.canScale"
-                  :autoCrop="option.autoCrop"
-                  :autoCropHeight="option.autoCropHeight"
-                  :auto-crop-width="option.autoCropWidth"
-                  :fixed="option.fixed"
-                  :fixed-number="option.fixedNumber"
+                <vueCropper class="vueCropper"
+                            ref="cropper"
+                            :img="option.img"
+                            :outputSize="option.outputSize"
+                            :outputType="option.outputType"
+                            :info="option.info"
+                            :canScale="option.canScale"
+                            :autoCrop="option.autoCrop"
+                            :autoCropHeight="option.autoCropHeight"
+                            :auto-crop-width="option.autoCropWidth"
+                            :center-box="option.centerBox"
+                            :can-move="option.canMove"
                 ></vueCropper>
               </div>
-              <Button  @click="">确认上传</Button>
             </div>
           </Modal>
-
         </i-col>
       </Row>
 
@@ -50,7 +51,8 @@
           return {
             file:null,
             modal:false,
-            loadingStatus: false,
+            loadingStatus: true,
+            uploadBase64:null,
             option:{
               img:'',                          // 裁剪图片的地址
               info: true,                      // 裁剪框的大小信息
@@ -58,12 +60,11 @@
               outputType:'png',                // 裁剪生成图片的格式
               canScale: false,                 // 图片是否允许滚轮缩放
               autoCrop: true,                  // 是否默认生成截图框
-              autoCropWidth: 150,              // 默认生成截图框宽度
-              autoCropHeight: 150,             // 默认生成截图框高度
-              fixed: false,                    // 是否开启截图框宽高固定比例
-              fixedNumber: [4, 4]              // 截图框的宽高比例
+              autoCropWidth: 200,              // 默认生成截图框宽度
+              autoCropHeight: 200,             // 默认生成截图框高度
+              centerBox:true,                  // 截图框是否被限制在图片里面
+              canMove:false,                   // 上传图片是否可以移动
             },
-            isShowCropper: false,            //是否显示截图框
             isShowModal:false,  //是否显示对话框
           }
       },
@@ -71,39 +72,52 @@
         handleUpload(file){
           console.log("handleUpload",file);
           this.file = file;
+
+          //创建一个FileReader 对象
+          let reader = new FileReader();
+          // readAsDataURL 方法用于读取指定 Blob 或 File 的内容
+          // 当读操作完成，readyState 变为 DONE，loadend 被触发，此时 result 属性包含数据：URL（以 base64 编码的字符串表示文件的数据）
+          // 读取文件作为 URL 可访问地址
+          reader.readAsDataURL(file);
+          reader.onload =() =>{
+            this.uploadBase64 = reader.result;
+            if (this.uploadBase64 !== null){
+              // console.log("this.uploadBase64 : ",this.uploadBase64);
+              this.option.img = reader.result;
+              this.isShowModal = true;
+            }
+          };
           return false;
         },
 
-        select_file(){
-          if (this.file !==null){
-            this.option.img = '/static/picture/avatar_default.jpg'
-            this.isShowModal = true
-          }else {
-            console.log("输出为空")
-          }
-
-        },
-
-        onCubeImg(){
-
+        upLoadFiles(){
+          alert(6666)
+          this.isShowModal = true;
         }
+
       },
     computed:{
       scrollHeight(){
-        return "height:" + ((window.screen.height)) + "px";
+        return "height:" + ((window.screen.height)*0.8) + "px;";
+      },
+      ModalHeight(){
+        return {
+          top:'0',
+          marginTop:'25px'
+        }
       }
     },
+
   }
 </script>
 
 <style scoped>
   .cropper-content{
-    position: fixed;
-    z-index: 199;
     width: 100%;
     height: 100%;
   }
   .cropper{
     width: 100%;
   }
+
 </style>
