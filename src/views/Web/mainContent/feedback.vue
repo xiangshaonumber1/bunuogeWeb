53a1063900305ca0ecd70119b5c1a3cee7a524f1
zhangjingxiang <821940979@qq.com>
2019-01-29 13:56:46 +0800

    用户反馈界面已完成， 接下来考虑的是 1.登录注册界面的登录和注册表单采用Iview的表单

--- src/views/Web/mainContent/feedback.vue
+++ src/views/Web/mainContent/feedback.vue
@@ -3,19 +3,24 @@
 <template>
     <div id="feedback">
       <Row type="flex" justify="center" align="center" class="code-row-bg">
-        <i-col span="12" style="border: 1px solid red;" class="text-center">
+        <i-col span="12" class="text-center">
           <img src="/static/picture/ok_blog_logo.png" style="height: 80px;" alt="img_none">
         </i-col>
       </Row>
 
       <Row type="flex" justify="center" align="center" class="code-row-bg">
-        <i-col span="12" style="border: 1px solid black">
+        <i-col span="12">
           <Divider orientation="left"><span style="font-weight: bold;font-size: 22px">意见反馈</span></Divider>
+          <div class="word-tip">
+            如果您有任何关于本站的意见、疑惑等，均可在此处写下您的宝贵意见。<br>
+            联系方式可不填，不填则默认为匿名建议，我们也无法联系您，如果您期待我的回复，不妨留下您的联系方式（推荐邮箱）<br>
+            本站由站主一人完成并主持维护，如果长时间没回复，可能是太忙了，望谅解。
+          </div>
         </i-col>
       </Row>
 
       <Row type="flex" justify="center" align="center" class="code-row-bg">
-        <i-col span="12" style="border: 1px solid fuchsia;">
+        <i-col span="12">
 
           <i-form :model="form_input" label-position="left" :label-width="150">
 
@@ -62,6 +67,10 @@
 
   span{
     font-size: 16px;
+  }
+
+  .word-tip{
+    padding-bottom: 15px;
   }
 
 </style>
