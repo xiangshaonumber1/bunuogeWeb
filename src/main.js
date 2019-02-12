c1530a2edd30b2e8f8585e0e5f8adcac7d64d88b
18482033263@sina.com <qq821940979>
2019-02-12 21:53:33 +0800

    axios设置了全局的请求（响应）拦截齐，记得要返回对应的response 或者 error等，不然，组件中的axios，无法处理response或者error

--- src/main.js
+++ src/main.js
@@ -53,36 +53,32 @@ import Web_writeArticle from './views/Web/mainContent/web_article_write'
 
 
 
-//axios 参数格式
-Vue.prototype.$qs = qs;
-//Vue 网络请求工具
-Vue.prototype.$axios = axios;  //全局注册，使用方法为：this.$axios
-//axios 全局默认配置
-// axios.defaults.baseURL = 'http://localhost:8888';
-const service = axios.create({
-  baseURL:'http://localhost:8888'
-});
+
+
+// const service = axios.create({
+//   baseURL:'http://localhost:8888'
+// });
 
 //添加请求拦截器
-service.interceptors.request.use(config=>{
-  //在发送请求之前做某事，比如设置token
-  console.log("在发送请求之前做某事，比如设置token");
-  config.headers["X-Auth-Token"] = this.$store.getters.userToken;
-  return config;
-},error => {
-  //请求错误时做些事情
-  console.log("请求失败");
-  return Promise.reject(error); //方法返回一个带有拒绝原因reason参数的Promise对象。
-});
+// service.interceptors.request.use(config=>{
+//   //在发送请求之前做某事，比如设置token
+//   console.log("在发送请求之前做某事，比如设置token");
+//   config.headers["X-Auth-Token"] = this.$store.getters.userToken;
+//   return config;
+// },error => {
+//   //请求错误时做些事情
+//   console.log("请求失败");
+//   return Promise.reject(error); //方法返回一个带有拒绝原因reason参数的Promise对象。
+// });
 
 //添加响应拦截器
-service.interceptors.response.use(response=>{
-  //对响应数据做些事
-  console.log("http 链接成功");
-},error => {
-  console.log("返回接口返回的错误信息");
-  return Promise.reject(error.response.data); // 返回接口返回的错误信息
-});
+// service.interceptors.response.use(response=>{
+//   //对响应数据做些事
+//   console.log("http 链接成功");
+// },error => {
+//   console.log("返回接口返回的错误信息");
+//   return Promise.reject(error.response.data); // 返回接口返回的错误信息
+// });
 
 
 //  2.定义路由
@@ -148,4 +144,33 @@ const app = new Vue({
 });//现在，可以试试启动喽
 
 
+//axios 参数格式
+Vue.prototype.$qs = qs;
+//Vue 网络请求工具
+Vue.prototype.$axios = axios;  //全局注册，使用方法为：this.$axios
+//axios 全局默认配置
+axios.defaults.baseURL = 'http://localhost:8888';
+//添加请求拦截器
+axios.interceptors.request.use(config=>{
+  //在发送请求之前做某事，比如设置token
+  console.log("在发送请求之前做某事，比如设置token",localStorage.getItem("vuex"));
+  config.headers["X-Auth-Token"] = localStorage.getItem("vuex").token;
+  return config;
+},error => {
+  //请求错误时做些事情
+  console.log("请求失败");
+  return Promise.reject(error); //方法返回一个带有拒绝原因reason参数的Promise对象。
+});
+
+//添加响应拦截器
+axios.interceptors.response.use((response)=>{
+  //对响应数据做些事
+  console.log("http 链接成功",response);
+  console.log("http 链接成功",response.data);
+  console.log("http 链接成功",response.data.code);
+  return response;
+},error => {
+  console.log("返回接口返回的错误信息");
+  return Promise.reject(error); // 返回接口返回的错误信息
+});
 
